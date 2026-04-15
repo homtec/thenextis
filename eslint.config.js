41d4f39bdfe5d57c6864b38dd54a7e866ff721b3
Thomas Hecker <hello@thomashecker.net>
2026-04-15 09:02:37 +0200

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { caughtErrorsIgnorePattern: '^_' }],
+    },
+  },
+];
